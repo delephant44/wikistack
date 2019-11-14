96aa3e40c609cc600f019c730aa88c5916ac14c2
Adel Jabbar <adeljabb@gmail.com>
2019-11-14 13:08:14 -0500

    finished schema

--- models/index.js
+++ models/index.js
@@ -2,11 +2,36 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
 const fun = () => {
-    console.log('FUN')
+  console.log('FUN')
 }
+
+const Page = db.define("page", {
+  title: {
+    type: Sequelize.STRING,
+  },
+  slug: {
+    type: Sequelize.STRING,
+  },
+  content: {
+    type: Sequelize.TEXT,
+  },
+  status: {
+    type: Sequelize.ENUM("open", "closed"), //the only possible values can be what we specified - in this case, open and closed
+  },
+})
+
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  }
+})
 
 module.exports = {
   db,
   fun
 }
+
 
