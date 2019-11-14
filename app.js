199a64bc095561553d6047966db8abb6406b7035
Adam Vare <adam44@me.com>
2019-11-14 16:06:35 -0500

    hopefully db sync is working

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-const express = require('express')
-const morgan = require('morgan')
-const routes = require('./routes/index')
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes/index');
 
-const app = express()
+const app = express();
 
-const { db, fun } = require('./models/index');//aquiring our database
+const { db } = require('./models/index'); //aquiring our database
 /* the above can be done like this:
 //const main = require('./models/index')
 //main.db
 //main.fun
 */
 
-db.authenticate().
-then(() => {
+db.authenticate().then(() => {
   console.log('connected to the database');
-})
+});
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-const PORT = 3000
+const PORT = 3000;
 
-app.use(express.urlencoded({extended:false}))
-
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('/public'));
 
+app.use('/', routes);
 
-app.use('/', routes)
+async function initializeSyncDatabase() {
+    //await db.sync({force: true})
+  await db.sync();
+  app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-    console.log(`app listening in port ${PORT}`)
-})
+initializeSyncDatabase();
 
-
-
-// Try getting your main layout to display when you visit http://localhost:3000. 
-// You can do this by passing an empty string to your layout 
+// Try getting your main layout to display when you visit http://localhost:3000.
+// You can do this by passing an empty string to your layout
 // function and res.sending it in your main route.
--- models/index.js
+++ models/index.js
@@ -31,7 +31,7 @@ const User = db.define("user", {
 
 module.exports = {
   db,
-  fun
+  
 }
 
 
